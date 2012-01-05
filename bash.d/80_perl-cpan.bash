dd580bbf2976211c9fb3004522a14db749f5f361
Michel Blanc <mb@mbnet.fr>
2012-01-05 19:23:55 +0100

    added CPAN stuff to bash & trying gnome-shell

---
+++ bash.d/80_perl-cpan.bash
@@ -0,0 +1,7 @@
+export PERL_LOCAL_LIB_ROOT="/home/leucos/perl5";
+export PERL_MB_OPT="--install_base /home/leucos/perl5";
+export PERL_MM_OPT="INSTALL_BASE=/home/leucos/perl5";
+export PERL5LIB="/home/leucos/perl5/lib/perl5/x86_64-linux-thread-multi:/home/leucos/perl5/lib/perl5";
+export PATH="/home/leucos/perl5/bin:$PATH";
+
+
